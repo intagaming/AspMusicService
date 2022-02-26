ff3dbe6d3757fddc2f65b083ce176cbb2a274c07
An Xuan Hoang <xuanan2001@gmail.com>
2022-02-26 11:06:36 +0700

    basic search

---
+++ Pages/Search.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MusicService.Data;
+using MusicService.Models;
+
+namespace MusicService.Pages
+{
+    public class SearchModel : PageModel
+    {
+        private readonly MusicService.Data.MusicContext _context;
+
+        public SearchModel(MusicService.Data.MusicContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Song> Song { get;set; }
+
+        public async Task OnGetAsync(string searchinput)
+        {
+            Song = await _context.Songs.Where(song => song.Name.ToLower().Contains(searchinput)).Include(song => song.Artists).ToListAsync();
+        }
+    }
+}
